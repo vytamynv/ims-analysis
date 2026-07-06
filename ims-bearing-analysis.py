e0266e2c3a51c7a2e3311532c5b6d15cfa371155
V. <vyta369@gmail.com>
2026-07-06 22:29:21 +0700

    Added IMS bearing analysis project

---
+++ ims-bearing-analysis.py
@@ -0,0 +1,15 @@
+import matplotlib
+import os
+import numpy as np
+import glob
+"""
+This project aims to analyze and predict the trend of IMS bearing from NASA.
+
+Source:
+https://data.nasa.gov/dataset/ims-bearings
+
+Author: Vytamyn
+cvn
+"""
+
+
